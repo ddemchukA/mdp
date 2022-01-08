6da7d5f43078bbefb0b772eb8b60d13d1ea265ac
ddemchukA <96911715+ddemchukA@users.noreply.github.com>
2022-01-08 14:47:08 +0700

    ADD_riskDor
    Добавлены риски дорожных эксцессов

--- hospital/bn.py
+++ hospital/bn.py
@@ -112,13 +112,19 @@ class podg:
         bn1=gum.BayesNet('MedRisk')
         riskdix = bn1.add(gum.LabelizedVariable ( 'riskdix' , 'itogdix', 2))
         bn1.cpt(riskdix).fillWith([0.0,0.0])
+        riskdor = bn1.add(gum.LabelizedVariable ( 'riskdor' , 'itogdor', 2))
+        bn1.cpt(riskdor).fillWith([0.0,0.0])
         komgl=bn1.add('komgl',4)
         tipdix=bn1.add('tipdix',3)
         satur=bn1.add('satur',3)
-        bn1=gum.fastBN("komgl[1,4]->riskdix[1,2];tipdix[1,3]->riskdix[1,2];satur[1,3]->riskdix[1,2]")
+        avto=bn1.add('avto',2)
+        roadrate=bn1.add('roadrate',3)
+        bn1=gum.fastBN("komgl[1,4]->riskdix[1,2];tipdix[1,3]->riskdix[1,2];satur[1,3]->riskdix[1,2];avto[1,2]->riskdor[1,2];roadrate[1,3]->riskdor[1,2];komgl[1,4]->riskdor[1,2];tipdix[1,3]->riskdor[1,2]")
         bn1.cpt("komgl")[:] = kk['glazg']
         bn1.cpt("tipdix")[:] = kk['dix']
         bn1.cpt("satur")[:] = kk['satur']
+        bn1.cpt("avto")[:] = kk['transp']
+        bn1.cpt("roadrate")[:] = kk['roadrate']
         #Нарушения дыхания
         bn1.cpt("riskdix")[{'satur':0, 'tipdix':0, 'komgl':0}] = [0.9,0.1]
         bn1.cpt("riskdix")[{'satur':0, 'tipdix':0, 'komgl':1}] = [0.9,0.1]
@@ -156,15 +162,102 @@ class podg:
         bn1.cpt("riskdix")[{'satur':2, 'tipdix':2, 'komgl':1}] = [0.3,0.7]
         bn1.cpt("riskdix")[{'satur':2, 'tipdix':2, 'komgl':2}] = [0.3,0.7]
         bn1.cpt("riskdix")[{'satur':2, 'tipdix':2, 'komgl':3}] = [0.3,0.7]
+        #Дорожные эксцессы
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':0, 'avto':0}] = [0.6,0.4]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':0, 'avto':1}] = [0.8,0.2]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':1, 'avto':0}] = [0.5,0.5]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':1, 'avto':1}] = [0.7,0.3]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':2, 'avto':0}] = [0.5,0.5]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':0, 'roadrate':1, 'avto':1}] = [0.6,0.4]
 
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':0, 'avto':0}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':0, 'avto':1}] = [0.6,0.4]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':1, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':1, 'avto':1}] = [0.5,0.5]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':2, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':1, 'roadrate':2, 'avto':1}] = [0.4,0.6]
 
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':0, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':0, 'avto':1}] = [0.4,0.6]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':1, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':1, 'avto':1}] = [0.4,0.6]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':2, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':2, 'roadrate':2, 'avto':1}] = [0.3,0.7]
+
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':0, 'avto':1}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':1, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':0, 'komgl':3, 'roadrate':2, 'avto':1}] = [0.2,0.8]
+
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':0, 'avto':0}] = [0.4,0.6]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':0, 'avto':1}] = [0.8,0.2]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':1, 'avto':0}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':1, 'avto':1}] = [0.8,0.2]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':2, 'avto':0}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':0, 'roadrate':2, 'avto':1}] = [0.7,0.3]
+
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':0, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':0, 'avto':1}] = [0.4,0.6]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':1, 'avto':0}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':1, 'avto':1}] = [0.4,0.6]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':2, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':1, 'roadrate':2, 'avto':1}] = [0.3,0.7]
+
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':0, 'avto':1}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':1, 'avto':1}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':2, 'roadrate':2, 'avto':1}] = [0.2,0.8]
+
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':0, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':0, 'avto':1}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':1, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':1, 'avto':1}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':1, 'komgl':3, 'roadrate':2, 'avto':1}] = [0.02,0.98]
+
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':0, 'avto':1}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':1, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':0, 'roadrate':2, 'avto':1}] = [0.2,0.8]
+
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':0, 'avto':1}] = [0.3,0.7]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':1, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':1, 'roadrate':2, 'avto':1}] = [0.2,0.8]
+
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':0, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':1, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':2, 'roadrate':2, 'avto':1}] = [0.2,0.8]
+
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':0, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':0, 'avto':1}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':1, 'avto':0}] = [0.1,0.9]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':1, 'avto':1}] = [0.2,0.8]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':2, 'avto':0}] = [0.02,0.98]
+        bn1.cpt("riskdor")[{'tipdix':2, 'komgl':3, 'roadrate':2, 'avto':1}] = [0.1,0.9]
 
         ie=gum.LazyPropagation(bn1)
         ie.makeInference()
         resu=ie.posterior('riskdix')
+        resu1=ie.posterior('riskdor')
         k=resu.tolist()
+        m=resu1.tolist()
         k1={}
         k1['riskdix']={}
+        k1['riskdor']={}
         k1['riskdix']['critical']=round(k[0],2)
         k1['riskdix']['non_critical']=round(k[1],2)
+        k1['riskdor']['Yes']=round(m[0],2)
+        k1['riskdor']['No']=round(m[1],2)
         return k1
